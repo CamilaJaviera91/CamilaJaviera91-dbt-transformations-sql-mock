8be2de831ff645ce766f19f367577c7252f8ceed
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-04-23 23:13:28 -0400

    feat(model): change SQL sentence so it can work without with

--- transformations/models/3. staff_changes.sql
+++ transformations/models/3. staff_changes.sql
@@ -1,17 +1,13 @@
 {{ config(materialized='table') }}
 
-WITH employees AS (
-    SELECT *
-    FROM {{ source('public', 'employees') }}) 
-
-    SELECT 
-        EXTRACT(YEAR FROM COALESCE(e.hire_date::DATE, e.termination_date::DATE)) AS year,
-        e.department,
-        COUNT(e.id) FILTER (WHERE e.hire_date IS NOT NULL) AS hired_count,
-        COUNT(e.id) FILTER (WHERE e.termination_date IS NOT NULL) AS terminated_count
-    FROM employees e
-    WHERE hire_date IS NOT NULL OR termination_date IS NOT NULL
-    GROUP BY 
-        EXTRACT(YEAR FROM COALESCE(hire_date::DATE, termination_date::DATE)), 
-        department
-    ORDER BY year
+SELECT 
+    EXTRACT(YEAR FROM COALESCE(e.hire_date::DATE, e.termination_date::DATE)) AS year,
+    e.department,
+    COUNT(e.id) FILTER (WHERE e.hire_date IS NOT NULL) AS hired_count,
+    COUNT(e.id) FILTER (WHERE e.termination_date IS NOT NULL) AS terminated_count
+FROM {{ source('public', 'employees') }} e
+WHERE e.hire_date IS NOT NULL OR e.termination_date IS NOT NULL
+GROUP BY 
+    EXTRACT(YEAR FROM COALESCE(e.hire_date::DATE, e.termination_date::DATE)), 
+    e.department
+ORDER BY year;
