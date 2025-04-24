ac0cff809f46c8d044897a6e3d388d2fc11a7837
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-04-23 23:12:43 -0400

    feat(model): change SQL sentence so it can work without with

--- transformations/models/1. dept_exits.sql
+++ transformations/models/1. dept_exits.sql
@@ -1,12 +1,8 @@
 {{ config(materialized='table') }}
 
-WITH employees AS (
-    SELECT *
-    FROM {{ source('public', 'employees') }}) 
-    
-    SELECT
-        e.department, 
-        COUNT(e.id) AS employees
-    FROM employees e
-    WHERE termination_date IS NOT NULL
-    GROUP BY department
+SELECT
+    e.department, 
+    COUNT(e.id) AS employees
+FROM {{ source('public', 'employees') }} e
+WHERE e.termination_date IS NOT NULL
+GROUP BY e.department;
