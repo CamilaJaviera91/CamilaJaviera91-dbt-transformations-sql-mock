41f6504b3f9517dcede2ae2ca37eb365386b3872
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-04-25 16:09:14 -0400

    feat(bq): add new profile and change sql sentence

--- transformations_bq/models/transformations_bigquery/employees_resume.sql
+++ transformations_bq/models/transformations_bigquery/employees_resume.sql
@@ -1,37 +1,37 @@
 {{ config(materialized='table') }}
 
-WITH employees_resume AS 
-(SELECT
+WITH employees_resume AS (
+  SELECT
     e.gender,
     e.department,
     e.job_title,
-    EXTRACT(YEAR FROM CURRENT_DATE()::DATE) AS current_year,
-    EXTRACT(YEAR FROM e.date_birth::DATE) AS birth_year,
-    EXTRACT(YEAR FROM e.hire_date::DATE) AS hired_year,
-    EXTRACT(YEAR FROM e.termination_date::DATE) AS termination_year,
+    EXTRACT(YEAR FROM CURRENT_DATE()) AS current_year,
+    EXTRACT(YEAR FROM DATE(e.date_birth)) AS birth_year,
+    EXTRACT(YEAR FROM DATE(e.hire_date)) AS hired_year,
+    EXTRACT(YEAR FROM DATE(e.termination_date)) AS termination_year,
     e.status,
     COUNT(e.id) AS employees
-FROM {{ source('public', 'employees') }} e  
-GROUP BY 
+  FROM {{ source('public', 'employees') }} e  
+  GROUP BY 
     e.gender,
     e.department,
     e.job_title,
-    EXTRACT(YEAR FROM now()::DATE),
-    EXTRACT(YEAR FROM e.date_birth::DATE),
-    EXTRACT(YEAR FROM e.hire_date::DATE),
-    EXTRACT(YEAR FROM e.termination_date::DATE),
+    EXTRACT(YEAR FROM CURRENT_DATE()),
+    EXTRACT(YEAR FROM DATE(e.date_birth)),
+    EXTRACT(YEAR FROM DATE(e.hire_date)),
+    EXTRACT(YEAR FROM DATE(e.termination_date)),
     e.status
-ORDER BY EXTRACT(YEAR FROM e.date_birth::DATE) ASC)
+)
 SELECT 
-    er.gender,
-    er.department,
-    er.job_title,
-    er.employees,
-    (er.current_year - er.birth_year) AS age,
-    CASE 
-        WHEN er.termination_year IS NULL THEN (er.current_year - er.hired_year)
-        ELSE (er.termination_year - er.hired_year)
-    END AS working_years,
-    er.status
+  er.gender,
+  er.department,
+  er.job_title,
+  er.employees,
+  (er.current_year - er.birth_year) AS age,
+  CASE 
+    WHEN er.termination_year IS NULL THEN (er.current_year - er.hired_year)
+    ELSE (er.termination_year - er.hired_year)
+  END AS working_years,
+  er.status
 FROM employees_resume er
 ORDER BY age DESC
