3921e0e4690120769462a02ee913e33064728d01
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-04-25 12:40:39 -0400

    feat(postgres): delete old files

--- transformations/models/1. dept_exits.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-    e.department, 
-    COUNT(e.id) AS employees
-FROM {{ source('public', 'employees') }} e
-WHERE e.termination_date IS NOT NULL
-GROUP BY e.department
